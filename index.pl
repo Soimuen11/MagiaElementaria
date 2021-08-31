ccc37837aedbbcca24077e20e0a6c2893bc7f8f9
Phil Wayne <soimuen11@gmail.com>
2021-08-31 22:45:35 +0200

    1. wrote a function to set player name and save it into a file 2. created a main menu 3. defined encoding as utf-8

--- index.pl
+++ index.pl
@@ -1,38 +1,60 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use open qw( :encoding(UTF-8) :std ); # Make UTF-8 default encoding
 
-sub main(){
-	print "Hello World! Could you please tell me your name:";
-	my $playerName = getPlayerName();
-	my $generalLocation = "Castle Of Hell";
-	my $location = "room 1";
-	my @room1Content = ("one chest");
+#sub main(){
+#	print "Hello World! Could you please tell me your name:";
+#	my $playerName = setPlayerName();
+#	my $generalLocation = "Castle Of Hell";
+#	my $location = "room 1";
+#	my @room1Content = ("one chest");
+#	my $gold = 0;
+#	my @playerItems = ();
+#
+#	# Find a sword, a shield , an armor and 3000 gold
+#	my @room1ChestContent = ("1 sword\n", "1 shield\n", "1 armor", "3000 gold\n");
+#	print "Welcome $playerName! You are in $location of the $generalLocation!\n";
+#	# Prompt the user to know if they wanna open the chest
+#	print "There is $room1Content[0], do you wish to open it?\n";
+#	print "1. Yes\n";
+#	print "2. No\n";
+#	my $answer = <>;
+#	if ($answer == 1){
+#		print "You have chosen to open the chest\n";
+#		print "You have obtained:\n @room1ChestContent\n";
+#		$gold += 3000;
+#		print "Congrats, you now have $gold gold.\n"
+#	}elsif ($answer == 2){
+#		print "So you would rather not open it... Not very courageous, eh?\n";
+#		print "The game is over for you.";
+#	}
+#}
 
-	# Find a sword, a shield and 3000 gold
-	my @room1ChestContent = ("1 sword\n", "1 shield\n", "3000 gold\n");
-	print "Welcome $playerName! You are in $location of the $generalLocation!\n";
-	# Prompt the user to know if they wanna open the chest
-	print "There is $room1Content[0], do you wish to open it?\n";
-	print "1. Yes\n";
-	print "2. No\n";
-	my $answer = <>;
-	if ($answer == 1){
-		print "You have chosen to open the chest\n";
-		print "You have obtained:\n @room1ChestContent\n";
-	}elsif ($answer == 2){
-		print "So you would rather not open it... Not very courageous, eh?\n";
-		print "The game is over for you.";
+sub startMenu(){
+	print "Welcome to the fantasy world of Magia Elementaria!\n";
+	my @options = ("1) Load Game\n", "2) New Game\n");
+	foreach my $option (@options){
+		print $option;
+	}
+	my $userChoice = <>;
+	if ($userChoice == 1){
+		print "Travel safe, experienced adventured!\n";
+	}elsif ($userChoice == 2){
+		print "A new adventure always sounds exciting.\n";
+	}else{
+		print "This is not a valid option.\n";
 	}
 }
 
 # Get player name and save it into a file
-sub getPlayerName(){
-	open (FH, ">", "save.txt");
-	my $playerName = <>;
-	print FH $playerName;
-	close(FH) or "Couldn't close the file"; 
-	return $playerName;
-}
+# sub setPlayerName(){
+# 	open (FH, ">", "save.txt");
+# 	my $playerName = <>;
+# 	print FH $playerName;
+# 	close(FH) or "Couldn't close the file"; 
+# 	return $playerName;
+# }
 
-main();
+# main();
+startMenu();
